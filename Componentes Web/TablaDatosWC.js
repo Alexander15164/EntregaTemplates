943226f062c858699b6ce1ef06e5435d8c93af8f
Alexander Monzon <alexandermm2011@gmail.com>
2018-05-01 08:36:41 -0600

    Web component terminado, pagina funcional

--- Componentes Web/TablaDatosWC.js
+++ Componentes Web/TablaDatosWC.js
@@ -26,31 +26,102 @@ class TablaDatosWC extends HTMLElement{
     var opcion10 = document.createElement('option');
     var opcion11 = document.createElement('option');
     var opcion12 = document.createElement('option');
+    var opcion13 = document.createElement('option');
+    var opcion14 = document.createElement('option');
+    var opcion15 = document.createElement('option');
+    var opcion16 = document.createElement('option');
+    var opcion17 = document.createElement('option');
+    var opcion18 = document.createElement('option');
+    var opcion19 = document.createElement('option');
+    var opcion20 = document.createElement('option');
+    var opcion21 = document.createElement('option');
+    var opcion22 = document.createElement('option');
+    var opcion23 = document.createElement('option');
     opcion0.setAttribute('value', 'seleccion');
     opcion0.innerText = 'Seleccione una entidad';
     select.appendChild(opcion0);
     opcion1.setAttribute('value', 'marca');
-    opcion1.innerText = ' Marca';
+    opcion1.innerText = 'Marca';
     select.appendChild(opcion1);
     opcion2.setAttribute('value', 'modelo');
-    opcion2.innerText = ' Modelo';
+    opcion2.innerText = 'Modelo';
     select.appendChild(opcion2);
     opcion3.setAttribute('value', 'unidad');
-    opcion3.innerText = ' unidad';
+    opcion3.innerText = 'Unidad';
     select.appendChild(opcion3);
+    opcion4.setAttribute('value', 'solicitud');
+    opcion4.innerText = 'Solicitud';
+    select.appendChild(opcion4);
+    opcion5.setAttribute('value', 'subTipoMantenimiento');
+    opcion5.innerText = 'Sub Tipo Mantenimiento';
+    select.appendChild(opcion5);
+    opcion6.setAttribute('value', 'tipoMantenimiento');
+    opcion6.innerText = ' Tipo Mantenimiento';
+    select.appendChild(opcion6);
+    opcion7.setAttribute('value', 'prioridad');
+    opcion7.innerText = ' Prioridad';
+    select.appendChild(opcion7);
+    opcion8.setAttribute('value', 'equipo');
+    opcion8.innerText = 'Equipo';
+    select.appendChild(opcion8);
+    opcion9.setAttribute('value', 'paso');
+    opcion9.innerText = 'Paso';
+    select.appendChild(opcion9);
+    opcion10.setAttribute('value', 'procedimiento');
+    opcion10.innerText = 'Procedimiento';
+    select.appendChild(opcion10);
+    opcion11.setAttribute('value', 'diagnostico');
+    opcion11.innerText = 'Diagnostico';
+    select.appendChild(opcion11);
     divEleccion.appendChild(select);
+    opcion12.setAttribute('value', 'diagnosticoParte');
+    opcion12.innerText = 'Diagnostico Parte';
+    select.appendChild(opcion12);
+    opcion13.setAttribute('value', 'parte');
+    opcion13.innerText = 'Parte';
+    select.appendChild(opcion13);
+    opcion14.setAttribute('value', 'tipoParte');
+    opcion14.innerText = 'Tipo Parte';
+    select.appendChild(opcion14);
+    opcion15.setAttribute('value', 'tipoResponsable');
+    opcion15.innerText = 'Tipo Responsable';
+    select.appendChild(opcion15);
+    opcion16.setAttribute('value', 'responsable');
+    opcion16.innerText = ' Responsable';
+    select.appendChild(opcion16);
+    opcion17.setAttribute('value', 'estado');
+    opcion17.innerText = 'Estado';
+    select.appendChild(opcion17);
+    opcion18.setAttribute('value', 'estadoMantenimientoDetalle');
+    opcion18.innerText = 'Estado Mantenimiento Detalle';
+    select.appendChild(opcion18);
+    opcion19.setAttribute('value', 'mantenimientoDetalle');
+    opcion19.innerText = 'Mantenimiento Detalle';
+    select.appendChild(opcion19);
+    opcion20.setAttribute('value', 'calendarioExcepcion');
+    opcion20.innerText = 'Calendario Excepcion';
+    select.appendChild(opcion20);
+    opcion21.setAttribute('value', 'ordenTrabajoCalendario');
+    opcion21.innerText = ' Orden Trabajo Calendario';
+    select.appendChild(opcion21);
+    opcion22.setAttribute('value', 'ordenTrabajo');
+    opcion22.innerText = 'Orden Trabajo';
+    select.appendChild(opcion22);
+    opcion23.setAttribute('value', 'equipoDetalle');
+    opcion23.innerText = 'Equipo Detalle';
+    select.appendChild(opcion23);
+
 
     divEleccion.setAttribute('id', 'seleccion');
     div.setAttribute('id', 'paginador');
     tabla.setAttribute('id', 'tablaEntidad');
     tabla.setAttribute('border', 1);
-    tabla.style.color = 'grey';
+    tabla.style.color = 'black';
     tabla.style.fontFamily = 'fantasy, Helvetica, sans-serif';
     select.onchange = function () {
       var selectedOption = this.options[select.selectedIndex];
       cabecerasDisponibles.style.background = '#D5E1F3';
       var nombre1 = selectedOption.value;
-      console.log(nombre1);
       var h2 = document.createElement('h2');
 
       Eabs.TodosLosDatos(nombre1).then(
@@ -71,8 +142,6 @@ class TablaDatosWC extends HTMLElement{
               cabecerasDisponibles.appendChild(h2);
 
               let cabeceras = Object.keys(data[0]);
-              console.log(cabecerasDisponibles);
-              console.log(cabeceras);
               for (let a = 0; a < cabeceras.length; a++) {
                 let check =  document.createElement('input');
                 let label =  document.createElement('label');
@@ -100,7 +169,6 @@ class TablaDatosWC extends HTMLElement{
                     x += 1;
                   }
                 }
-                console.log(Cabeceras);
 
                 while (tabla.firstChild) {
                   tabla.removeChild(tabla.firstChild);
@@ -129,12 +197,11 @@ class TablaDatosWC extends HTMLElement{
                     var numero = Object.keys(data[0]).length;
                     var contenido = Object.keys(data[0]);
 
-                    console.log(contenidoMod);
                     var tr = document.createElement('tr');
                     for (var j = 0; j < Cabeceras.length; j++) {
                       var td = document.createElement('td');
                       td.innerHTML = Cabeceras[j].toUpperCase();
-                      td.style.font ="italic bold 20px Verdana,serif";
+                      td.style.font = 'italic bold 20px Verdana,serif';
                       tr.appendChild(td);
                     }
 
@@ -214,7 +281,6 @@ class TablaDatosWC extends HTMLElement{
                     break;
                   }
 
-  /////////////////////////////////////////
                 }
 
               };
@@ -234,7 +300,7 @@ class TablaDatosWC extends HTMLElement{
   }
 
 }
-customElements.define('tabla-marca', TablaDatosWC);
+customElements.define('tabla-datos', TablaDatosWC);
 
 // El primer parametro es el nombre del Web ComponentesWeb
 // El segundo parametro es el nombre de la clase donde se define
--- TemplatesScripts/templateBase.js
+++
@@ -1,5 +0,0 @@
-let template = document.querySelector('#manTemplate');
-let clone = template.content.cloneNode(true);
-let  nombre  = clone.querySelector('nombre-tabla');
-nombre.setAttribute('leyenda', 'Marca');
-template.parentNode.appendChild(clone);
